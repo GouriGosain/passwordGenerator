1d9ae5953fa2675e43768e63a414a0eb8a47f906
Gouri Gosain <gouri.gosain@seamless.se>
2024-08-27 15:21:45 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+
+    content: [
+      "./src/**/*.{js,jsx,ts,tsx}", // Adjust paths based on your file structure
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+
+
